2624b1617000c9d9f61b69ab0425481ba4a50b32
Fabulotus <Fabulotus@users.noreply.github.com>
2018-09-17 21:36:48 +0300

    Replaced JS code to JQuery
    Now filter is working.

--- Фильтр/JS.js
+++ Фильтр/JS.js
@@ -1,29 +1,38 @@
-function reset() {
-    document.getElementById('red').checked = false;
-    document.getElementById('green').checked = false;
-    document.getElementById('blue').checked = false;
-    document.getElementById('black').checked = false;
-    document.getElementById('hero').checked = false;
-    document.getElementById('spell').checked = false;
-    document.getElementById('creep').checked = false;
-    for (var i = 0; i < 12; i++) {
-        ce = document.getElementById(i + 1);
-        ce.style.display = 'block';
+$(document).ready(function(){
+$('input[type="checkbox"]').click(function() {
+    
+     if ($('input[type="checkbox"]:checked').length > 0) {
+        $('.card').hide();
+        $('input[type="checkbox"]' && 'input[class="1"]:checked').each(function() {
+            var idk=this.id;
+            $('.'+idk).show();
+        if($('input[type="checkbox"]' && 'input[class="1"]:checked').length==0) {$('.card').show();}
+        $('input[type="checkbox"]' && 'input[class="2"]:not(:checked)').each(function() {
+        var idk=this.id;
+        $('.'+idk).hide();
+        if($('input[type="checkbox"]' && 'input[class="2"]:checked').length==0) {wow()}
+    //         $('#main-div[class=' + this.id + ']').show();
+    //     });
+    // } else { 
+    //     $('#main-div').show();
+        });
+     });}
+     if ($('input[type="checkbox"]:checked').length == 0) {reset()};
+});
+});
+function wow(){
+    $('.card').show();
+    if ($('input[type="checkbox"]:checked').length > 0) {
+        $('.card').hide();
+        $('input[type="checkbox"]' && 'input[class="1"]:checked').each(function() {
+            var idk=this.id;
+            $('.'+idk).show();
+});
     }
 }
-function filtera(b) {
-    var cardsid = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-    if (!document.getElementById(b).checked) {
-        cardsid.forEach(function (val, i, arr) {
-            if (!document.getElementById(val).className.includes(b)) {
-                document.getElementById(val).style.display = 'block';
-            }
-        })
-    } else {
-        cardsid.forEach(function (val, i, arr) {
-            if (!document.getElementById(val).className.includes(b)) {
-                document.getElementById(val).style.display = 'none';
-            }
-        })
-    }
+function reset() {
+    $('input[type="checkbox"]').each(function() {
+        this.checked=false;
+        $('.card').show();
+});
 }
