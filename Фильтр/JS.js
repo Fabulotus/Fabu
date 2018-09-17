4da5e6f4ca29596083d8a76d2d2ae9af81c56fd5
Fabulotus <Fabulotus@users.noreply.github.com>
2018-09-17 21:50:05 +0300

    Improved logic and code

--- Фильтр/JS.js
+++ Фильтр/JS.js
@@ -6,19 +6,15 @@ $('input[type="checkbox"]').click(function() {
         $('input[type="checkbox"]' && 'input[class="1"]:checked').each(function() {
             var idk=this.id;
             $('.'+idk).show();
+        });
         if($('input[type="checkbox"]' && 'input[class="1"]:checked').length==0) {$('.card').show();}
         $('input[type="checkbox"]' && 'input[class="2"]:not(:checked)').each(function() {
         var idk=this.id;
         $('.'+idk).hide();
-        if($('input[type="checkbox"]' && 'input[class="2"]:checked').length==0) {wow()}
-    //         $('#main-div[class=' + this.id + ']').show();
-    //     });
-    // } else { 
-    //     $('#main-div').show();
         });
-     });}
+        if($('input[type="checkbox"]' && 'input[class="2"]:checked').length==0) {wow()}
+    }
      if ($('input[type="checkbox"]:checked').length == 0) {reset()};
-});
 });
 function wow(){
     $('.card').show();
@@ -36,3 +32,4 @@ function reset() {
         $('.card').show();
 });
 }
+});
